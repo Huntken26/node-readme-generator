99a652ba948cff6965db14a98f5548fcfb50b15d
huntken26 <huntken26@gmail.com>
2022-11-01 01:31:46 -0500

    Generate readme function with the readme markup, write file function and inquirer prompt

--- Develop/index.js
+++ Develop/index.js
@@ -1,12 +1,121 @@
-// TODO: Include packages needed for this application
+//Included packages neccessary for this project
 const fs = require("fs");
 const inquirer = require("inquirer");
 
-// TODO: Create an array of questions for user input
-const questions = [];
+//  Added the markup needed for the generate readme function. Also added the inquirer prompt with the specific user questions
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const generateReadme = ({
+  title,
+  description,
+  contents,
+  installation,
+  usage,
+  credit,
+  license,
+  badges,
+  features,
+  contact,
+}) =>
+  `# ${title}:
+
+## DESCRIPTION:
+
+${description}
+
+## TABLE OF CONTENTS:
+
+${contents}
+
+## INSTALLATION:
+
+${installation}
+
+## USAGE/INSTRUCTIONS:
+
+${usage}
+
+## CREDITS:
+
+${credit}
+
+## LICENSES:
+
+${license}
+
+## BADGES:
+
+${badges}
+
+## FEATURES:
+
+${features}
+
+## HOW TO CONTRIBUTE:
+
+${contact}
+
+`;
+
+inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "What Is the title for this project?",
+      name: "title",
+    },
+    {
+      type: "input",
+      message: "Provide a short description of this project:",
+      name: "description",
+    },
+    {
+      type: "input",
+      message: "List the table of contents:",
+      name: "contents",
+    },
+    {
+      type: "input",
+      message: "How do you install the application?",
+      name: "installation",
+    },
+    {
+      type: "input",
+      message: "List the instructions for usage:",
+      name: "usage",
+    },
+    {
+      type: "input",
+      message: "Do you need to credit any sites or users?",
+      name: "credit",
+    },
+    {
+      type: "input",
+      message: "What License did you use:",
+      name: "license",
+    },
+    {
+      type: "input",
+      message: "Do you have any badges?",
+      name: "badges",
+    },
+    {
+      type: "input",
+      message: "Does your project have any special features?",
+      name: "features",
+    },
+    {
+      type: "input",
+      message: "How can one contribute to your project?",
+      name: "contact",
+    },
+  ])
+  .then((answers) => {
+    const readmeMarkup = generateReadme(answers);
+
+    fs.writeFile("README.md", readmeMarkup, (err) =>
+      err ? console.log(err) : console.log("Successfully created readme file!")
+    );
+  });
 
 // TODO: Create a function to initialize app
 function init() {}
